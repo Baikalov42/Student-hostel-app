64acbf8304f000f31be2591d301212b1b8788721
Vladimir Baykalov <Baikalov.v42@yandex.ru>
2020-08-12 15:14:05 +0300

    add Main class

--- src/main/java/ua/com/foxminded/studenthostel/StudentHostelApp.java
+++ src/main/java/ua/com/foxminded/studenthostel/StudentHostelApp.java
@@ -1,15 +1,6 @@
 package ua.com.foxminded.studenthostel;
 
-import ua.com.foxminded.studenthostel.models.*;
-
-import java.util.List;
-
 public class StudentHostelApp {
-
-    private List<Student> students;
-    private List<Task> tasks;
-    private List<Equipment> equipments;
-    private List<Room> rooms;
 
     public static void main(String[] args) {
 
---
+++ src/main/java/ua/com/foxminded/studenthostel/models/StudentHostel.java
@@ -0,0 +1,15 @@
+package ua.com.foxminded.studenthostel.models;
+
+import java.util.List;
+
+public class StudentHostel {
+
+    private List<Student> students;
+    private List<Task> tasks;
+    private List<Equipment> equipments;
+    private List<Room> rooms;
+
+    public static void main(String[] args) {
+
+    }
+}
