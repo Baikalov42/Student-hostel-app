3dbe77beae39c0204ed1f2f9e81795783b07b431
Vladimir Baykalov <Baikalov.v42@yandex.ru>
2020-08-12 15:36:34 +0300

    add enum names

--- src/main/java/ua/com/foxminded/studenthostel/models/Faculty.java
+++ src/main/java/ua/com/foxminded/studenthostel/models/Faculty.java
@@ -1,9 +1,15 @@
 package ua.com.foxminded.studenthostel.models;
 
 public enum Faculty {
-    WEB_DESIGN,
-    DATABASE_ARCHITECTURE,
-    GAME_DEVELOPMENT,
-    FRONT_END_DEVELOPMENT,
-    BECK_END_DEVELOPMENT,
+    WEB_DESIGN("web design"),
+    DATABASE_ARCHITECTURE("database architecture"),
+    GAME_DEVELOPMENT("game development"),
+    FRONT_END_DEVELOPMENT("front end development"),
+    BECK_END_DEVELOPMENT("beck end development");
+
+    private final String name;
+
+    Faculty(String name) {
+        this.name = name;
+    }
 }
